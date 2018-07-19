d722fb34d227cdd2004172556fa2e52c2922380a
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-19 15:36:58 +0000

    automatically backed up by learn

--- db/migrate/user.rb
+++ db/migrate/user.rb
@@ -4,7 +4,7 @@ class CreateUserTable < ActiveRecord::Migration[4.2]
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.float :balance
+      t.decimal :balance
     end
   end
 end
