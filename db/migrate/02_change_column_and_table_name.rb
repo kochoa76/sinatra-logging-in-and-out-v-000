7f2127b5c5d15d626208f01d47997168a15ccafe
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-19 15:46:34 +0000

    automatically backed up by learn

--- db/migrate/02_change_column_and_table_name.rb
+++ db/migrate/02_change_column_and_table_name.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[4.2]
 
-  def change 
+  def change
     create_table :users do |t|
-      t.string :username 
-      t.string :password 
-      t.decimal :balance 
-    end 
-  end 
+      t.string :username
+      t.string :password
+      t.decimal :balance
+    end
+  end
 end 
